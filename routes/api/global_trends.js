068810cb59100c5bb1cd4ceff44c5f8d445d99a8
jonathanbridges <jbridges7@gmail.com>
2019-07-18 13:35:13 -0700

    Add Twitter API call

--- routes/api/global_trends.js
+++ routes/api/global_trends.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const keys = require("../../config/keys");
+const axios = require("axios");
 
 router.get("/test", (req, res) => res.json({ msg: "This is the global trends route" }));
+
+router.get('/global_trends', (req, res) => {
+
+  const getTweets = () => {
+    let url = `https://api.twitter.com/1.1/trends/place.json?id=2487956`;
+    let token = keys.twitterToken;
+    return axios.get(url, { headers: { "Authorization": `Bearer ${token}` } })
+      .then(response => response.data);
+  };
+
+  getTweets().then(data => {
+    res.json({
+      message: "Request received!",
+      data
+    })
+  })
+
+});
 
 module.exports = router;
