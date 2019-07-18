cd1f158a186ad2efaf06239dfa8bf06238c8bf06
jonathanbridges <jbridges7@gmail.com>
2019-07-18 11:31:05 -0700

    Add Express Server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const global_trends = require("./routes/api/global_trends")
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.send("Hello World"));
+app.use("/api/global_trends", global_trends)
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  twitterToken: 'AAAAAAAAAAAAAAAAAAAAAJ4i%2FQAAAAAABdhZf64UCxVo7Mm3Y2vAl5khzJo%3DAen6pVGbhSo9zbHnOOkNf3gJEivFc5bnKV7hYU6hGW9fzx2gNI'
+}
